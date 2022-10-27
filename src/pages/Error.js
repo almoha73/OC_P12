4c222c6dc31e3434fe831d03e611dfa71dafdb20
Agnes <almoha@gmail.com>
2022-10-27 13:02:25 +0200

    stylized error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import ErrorPieChart from '../Components/ErrorPieChart';
 
 const Error = () => {
     return (
-        <div>
-            <h1>ERROR</h1>
+        <div className="error-content">
+            <ErrorPieChart />
+            <Link to="/">Retour à la page d'accueil</Link>
         </div>
     );
 };
